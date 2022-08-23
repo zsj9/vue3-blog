9cabf8cf90a67b2786d7ac9ada204d882642eecb
zsj7012 <zsj7012>
2022-08-23 19:16:28 +0800

    tj

--- api/proxy.js
+++ api/proxy.js
@@ -6,18 +6,13 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 api 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    target = 'https://node-server-puce.vercel.app'
+    target = 'https://node-server-puce.vercel.app/api'
   }
 
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 例如 /api/user/login 将被转发到 https://node-server-puce.vercel.app/user/login
-      '^/api/': '/'
-    }
   })(req, res)
 }
 
